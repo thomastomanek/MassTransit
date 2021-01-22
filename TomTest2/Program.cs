3a15d3462d693bc6e3cc7fb316d1d6b2689063f1
Thomas Tomanek <thomas.tomanek@starlizard.uk>
2021-01-22 13:21:08 +0000

    Auto stash before rebase of "Upstream/develop"

---
+++ TomTest2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using GreenPipes;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MTTEst
+{
+    using MassTransit.Monitoring.Health;
+
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Host.CreateDefaultBuilder()
+                .ConfigureLogging(l =>
+               {
+                   l.ClearProviders();
+                   l.AddConsole();
+               })
+                .ConfigureServices(s =>
+               {
+                   s.AddMassTransit(c =>
+                   {
+                       c.UsingRabbitMq((cx, rx) =>
+                       {
+                            rx.Host("rabbitmq://localhost", hc =>
+                            {
+                                hc.Username("cricket");
+                                hc.Password("cricket");
+                            });
+
+                            rx.ReceiveEndpoint("test-receive-endpoint", re =>
+                            {
+                                re.PrefetchCount = 1;
+                                re.UseConcurrencyLimit(1);
+                                re.Handler<TestMessage>(async context =>
+                                {
+                                    Console.WriteLine("Got it");
+                                    await Task.Delay(TimeSpan.FromSeconds(5));
+                                });
+                            });
+
+                       });
+
+                   });
+                   s.AddMassTransitHostedService();
+               }).RunConsoleAsync();
+        }
+    }
+
+    class TestMessage
+    {
+
+    }
+
+}
---
+++ src/Tomtest/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using GreenPipes;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MTTEst
+{
+    using MassTransit.Monitoring.Health;
+
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Host.CreateDefaultBuilder()
+                .ConfigureLogging(l =>
+               {
+                   l.ClearProviders();
+                   l.AddConsole();
+               })
+                .ConfigureServices(s =>
+               {
+                   s.AddMassTransit(c =>
+                   {
+                       c.UsingRabbitMq((cx, rx) =>
+                       {
+                            rx.Host("rabbitmq://localhost", hc =>
+                            {
+                                hc.Username("cricket");
+                                hc.Password("cricket");
+                            });
+
+                       });
+
+                   });
+                   s.AddMassTransitHostedService();
+                   s.AddHostedService<RequestService>();
+               }).RunConsoleAsync();
+        }
+    }
+
+
+    class RequestService : IHostedService
+    {
+        readonly IBus _bus;
+        readonly IBusHealth _busHealth;
+
+        public RequestService(IBus bus, IBusHealth busHealth)
+        {
+            _bus = bus;
+            _busHealth = busHealth;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+
+            await Task.Factory.StartNew(async () =>
+            {
+                for (var i = 0; i < int.MaxValue; i++)
+                {
+                    try
+                    {
+                        await WaitForHealthyBus(cancellationToken);
+                        await _bus.Publish(new TestMessage());
+                    }
+                    catch (Exception e)
+                    {
+
+                    }
+
+                    Thread.Sleep(100);
+                }
+            });
+
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+        }
+
+        async Task WaitForHealthyBus(CancellationToken cancellationToken)
+        {
+            HealthResult result;
+            do
+            {
+                result = _busHealth.CheckHealth();
+
+                await Task.Delay(100, cancellationToken);
+            } while (result.Status != BusHealthStatus.Healthy);
+        }
+    }
+
+    class TestMessage
+    {
+
+    }
+
+}
